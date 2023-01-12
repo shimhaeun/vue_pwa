a99bbac0b94d87aae06291000ef548aabfc4b0e7
shimhaeun <80249641+shimhaeun@users.noreply.github.com>
2023-01-12 13:16:15 +0900

    Add files via upload
    0112

---
+++ pwa/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+      <v-main>
+          <!-- fill-height는 브라우저 높이를 100%, 수직으로 가운데 정렬 -->
+          <v-container fluid fill-height>
+              <v-row>
+                  <!-- text-center는 수평 가운데 정렬 -->
+                  <v-col cols="12" class="text-center">
+                      <!-- 타이포 스타일은 title, 글자색은 흰색으로 설정  -->
+                      <h1 class="title white--text">Hello!!!</h1>
+                      <p class="caption">by VueJS</p>
+                      <img src="./assets/hello-pwa.png" alt="" />
+                  </v-col>
+              </v-row>
+          </v-container>
+      </v-main>
+  </v-app>
+</template>
+
+
+<script>
+  export default {
+    name: 'App',
+    created () {
+        // 배경색을 다크모드 설정
+        this.$vuetify.theme.dark = true;
+    }
+  }
+</script>
